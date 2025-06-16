eb5e06ffcf988244137834b4109877947bc7979a
monokkai <egormayer08@mail.ru>
2025-06-17 00:41:09 +0200

    dev: configuring jwt tokens in main file

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using System.Text;
+using auth_service.Services;
+using auth_service.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 namespace auth_service;
 
 public class Program
@@ -5,9 +11,34 @@ public class Program
     public static void Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseMySql(
+                builder.Configuration.GetConnectionString("DefaultConnection"),
+                new MySqlServerVersion(new Version(8, 0, 3))
+            ));
+
         builder.Services.AddControllersWithViews();
-        builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+        builder.Services.AddAuthentication("Bearer").AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"])),
+                ValidateIssuer = true,
+                ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                ValidateAudience = true,
+                ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+        });
         builder.Services.AddAuthorization();
+
+        builder.Services.AddScoped<IAuthService, AuthService>();
+        builder.Services.AddScoped<ITokenService, TokenService>();
+        builder.Services.AddScoped<IPasswordService, PasswordService>();
+
         WebApplication app = builder.Build();
         app.UseHttpsRedirection();
         app.UseStaticFiles();
@@ -15,8 +46,8 @@ public class Program
         app.UseAuthentication();
         app.UseAuthorization();
         app.MapControllers();
-        
-        
+
+
         app.Run();
     }
 }
