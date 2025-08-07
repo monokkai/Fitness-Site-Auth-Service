a5fc4294bbf48fbd7ebca0dd3796c125dd8c124d
monokkai <egormayer08@mail.ru>
2025-08-07 15:11:45 +0200

    dev: CI/CD #4

--- Models/DTOs/AuthResultDto.cs
+++ Models/DTOs/AuthResultDto.cs
@@ -3,7 +3,7 @@ namespace auth_service.Models.DTOs;
 public class AuthResultDto
 {
     // Checking for CI/CD Pipeline
-    // Check 3
+    // Check 4
     public bool Success { get; set; }
     public string? Token { get; set; }
     public UserDto? User { get; set; }
