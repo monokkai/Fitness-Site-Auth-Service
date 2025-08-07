ea773800d36900a0006a297148fca8d94d9d99ff
monokkai <egormayer08@mail.ru>
2025-08-07 15:02:38 +0200

    dev: added CI/CD pipeline #2

--- Models/DTOs/AuthResultDto.cs
+++ Models/DTOs/AuthResultDto.cs
@@ -3,6 +3,7 @@ namespace auth_service.Models.DTOs;
 public class AuthResultDto
 {
     // Checking for CI/CD Pipeline
+    // Check 2
     public bool Success { get; set; }
     public string? Token { get; set; }
     public UserDto? User { get; set; }
